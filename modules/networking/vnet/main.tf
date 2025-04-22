554a42c46b3842d197bd9e3d95ce085f7ec88c87
Thisisashu <ashuagra481990@gmail.com>
2025-04-22 10:59:34 +0530

    added public ip and nics

--- modules/networking/vnet/main.tf
+++ modules/networking/vnet/main.tf
@@ -13,3 +13,25 @@ resource "azurerm_subnet" "network_subnets" {
   address_prefixes = [cidrsubnet(var.vnet_address_prefix,8,count.index)]
 
 }
+resource "azurerm_public_ip" "public_ipaddress" {
+  count = var.public_ip_address_count
+  name = "public-ip${count.index+1}"
+  resource_group_name = var.resource_group_name
+  location = var.location
+  allocation_method = "Static"
+}
+
+resource "azurerm_network_interface" "network_interfaces" {
+  count = var.netwrok_interface_count
+  name = "interface-0${count.index+1}"
+  location = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name = "internal"
+    subnet_id = azurerm_subnet.network_subnets[count.index].id 
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.public_ipaddress[count.index].id
+  }
+}
+
